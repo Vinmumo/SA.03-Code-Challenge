43f4508561388239ab40f5abec14ec25d5b39cd3
vincent <vinnymummo@gmail.com>
2024-04-08 09:15:30 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 fetch('http://localhost:3000/films')
 .then(res => res.json())
-.then(data => data.forEach((film, index) => { 
+.then(data => { 
+  data.forEach(film => { 
 //    console.log(film)
 
-     let title = document.querySelector("#films")
+     let titles = document.querySelector("#item")
     const mov = document.createElement('li')
-    mov.innerHTML =`
-   <button class="btn"  > ${film.title}</button>
+    mov.innerHTML=`
+    <button id ="${film.id}" onclick="showDetails()">${film.title}</button>
     `
-    title.appendChild(mov)
-    const buttn = document.querySelector('.btn')
+    titles.appendChild(mov)
+   
+})})
+
+function showDetails(){
+  fetch('http://localhost:3000/films')
+  .then(res => res.json())
+  .then(data => { 
+    data.forEach(film => { 
+      const buttn = document.getElementById(`${film.id}`)
     buttn.addEventListener('click', e => {
                 e.preventDefault()
                 let cardd = document.querySelector('#showing') 
@@ -30,6 +39,29 @@ fetch('http://localhost:3000/films')
               </button>
             </div>
             `
-})}))
-
-
+      const img = document.querySelector('#poster')
+      img.src = `${film.poster}`
+  })})})}
+// textContent = film.title
+//     const buttn = document.querySelector('.btn')
+//     buttn.addEventListener('click', e => {
+//                 e.preventDefault()
+//                 let cardd = document.querySelector('#showing') 
+//             let availTickets = `${film.capacity}`- `${film.tickets_sold}`
+//             cardd.innerHTML=`
+//             <div class="card">
+//             <div id="title" class="title">${film.title}</div>
+//             <div id="runtime" class="meta">${film.runtime} minutes</div>
+//             <div class="content">
+//               <div class="description">
+//                 <div id="film-info">${film.description}</div>
+//                 <span id="showtime" class="ui label">${film.showtime}</span>
+//                 <span id="ticket-num">${availTickets}</span> remaining tickets
+//               </div>
+//               <div class="extra content">
+//               <button id="buy-ticket" class="ui orange button">
+//                 Buy Ticket
+//               </button>
+//             </div>
+//             `
+// })})
