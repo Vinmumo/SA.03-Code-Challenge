156e8ac9309758115e91eaf75aab7b2c2099a7f9
vincent <vinnymummo@gmail.com>
2024-04-08 15:20:43 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ function showDetails(){
     data.forEach(film => { 
       const buttn = document.getElementById(`${film.id}`)
     buttn.addEventListener('click', e => {
-                e.preventDefault()
+                
                 let cardd = document.querySelector('#showing') 
             let availTickets = `${film.capacity}`- `${film.tickets_sold}`
             cardd.innerHTML=`
@@ -42,26 +42,5 @@ function showDetails(){
       const img = document.querySelector('#poster')
       img.src = `${film.poster}`
   })})})}
-// textContent = film.title
-//     const buttn = document.querySelector('.btn')
-//     buttn.addEventListener('click', e => {
-//                 e.preventDefault()
-//                 let cardd = document.querySelector('#showing') 
-//             let availTickets = `${film.capacity}`- `${film.tickets_sold}`
-//             cardd.innerHTML=`
-//             <div class="card">
-//             <div id="title" class="title">${film.title}</div>
-//             <div id="runtime" class="meta">${film.runtime} minutes</div>
-//             <div class="content">
-//               <div class="description">
-//                 <div id="film-info">${film.description}</div>
-//                 <span id="showtime" class="ui label">${film.showtime}</span>
-//                 <span id="ticket-num">${availTickets}</span> remaining tickets
-//               </div>
-//               <div class="extra content">
-//               <button id="buy-ticket" class="ui orange button">
-//                 Buy Ticket
-//               </button>
-//             </div>
-//             `
-// })})
+
+  const buy = document.querySelector('')
