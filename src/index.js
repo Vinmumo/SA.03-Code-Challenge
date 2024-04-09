edf95cc17ac056abc44c94b48b573029a8d7a197
vincent <vinnymummo@gmail.com>
2024-04-09 14:59:35 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/films')
+fetch('https://json-server-zeyw.onrender.com/films')
 .then(res => res.json())
 .then(data => { 
   data.forEach(film => { 
@@ -8,13 +8,19 @@ fetch('http://localhost:3000/films')
     const mov = document.createElement('li')
     mov.innerHTML=`
     <button id ="${film.id}" onclick="showDetails()">${film.title}</button>
+    <button id="del" >delete</button>
     `
     titles.appendChild(mov)
-   
+  
+      mov.querySelector('#del').addEventListener("click", () =>{
+        mov.remove()
+        
+       })
+
 })})
 
 function showDetails(){
-  fetch('http://localhost:3000/films')
+  fetch('https://json-server-zeyw.onrender.com/films')
   .then(res => res.json())
   .then(data => { 
     data.forEach(film => { 
@@ -31,10 +37,10 @@ function showDetails(){
               <div class="description">
                 <div id="film-info">${film.description}</div>
                 <span id="showtime" class="ui label">${film.showtime}</span>
-                <span id="ticket-num">${availTickets}</span> remaining tickets
+                <span id="ticket-num">${availTickets}remaining tickets</span> 
               </div>
               <div class="extra content">
-              <button id="buy-ticket" class="ui orange button">
+              <button id="buy-ticket" class="ui orange button" onclick="availTickets(-1)">
                 Buy Ticket
               </button>
             </div>
@@ -43,4 +49,15 @@ function showDetails(){
       img.src = `${film.poster}`
   })})})}
 
-  const buy = document.querySelector('')
+function availTickets(click){ 
+  const span = document.getElementById('ticket-num')
+  const value = parseInt(span.innerHTML)
+  span.innerHTML = value 
+  
+  if(value > 1){
+    span.innerHTML = value + click + 'remaining tickets'
+  }else {
+    span.textContent = "sold out"
+  }
+  
+}
